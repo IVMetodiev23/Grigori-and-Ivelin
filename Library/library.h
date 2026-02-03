e23c114494fd3e533ddf9bf3849b6038e986d011
IVMetodiev23 <IVMetodiev23@gmail.com>
2026-02-03 09:57:38 +0200

    Create static library project and function declarations

---
+++ Library/library.h
@@ -0,0 +1,23 @@
+#pragma once
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Book {
+    string title;
+    string author;
+    int year;
+    double price;
+};
+struct Library {
+    string name;
+    Book* books;
+    int count;
+};
+
+Book createBook(string title, string author, int year, double price);
+void printBook(Book book);
+void printLibrary(Library lib);
+double averageBookPrice(Library lib);
+Book oldestBookInLibrary(Library lib);
---
+++ Project1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../Library/library.h"
+using namespace std;
+
+int main() {
+
+}
